ba7506e393fef593175344410707bca0e6184080
DavihZ-GabrielP <davihzjapao@gmail.com>
2026-08-03 08:57:17 -0300

    Criação de controller, enuns e entidades

---
+++ src/main/java/br/com/etec/ingresso/entity/Filme.java
@@ -0,0 +1,30 @@
+package br.com.etec.ingresso.entity;
+
+import br.com.etec.ingresso.enuns.CategoriaFilmeEnum;
+import br.com.etec.ingresso.enuns.ClassificacaoIndicativaEnum;
+import br.com.etec.ingresso.enuns.SimNaoEnum;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+
+public class Filme {
+    private Long id;
+    private String nome;
+    private Integer duracao;
+    private CategoriaFilmeEnum categoria;
+    private ClassificacaoIndicativaEnum classificacao;
+    private Integer ano;
+    private String capa;
+    private String diretor;
+    private String elenco;
+    private String descricao;
+    private Double avaliacao;
+    private SimNaoEnum emCartaz;
+    private LocalDateTime dataExclusao;
+}
---
+++ src/main/java/br/com/etec/ingresso/enuns/SimNaoEnum.java
@@ -0,0 +1,6 @@
+package br.com.etec.ingresso.enuns;
+
+public enum SimNaoEnum {
+    S,
+    N;
+}
