0f2fba6dda77be4112a71401ee4a322da0befc65
DavihZ-GabrielP <davihzjapao@gmail.com>
2026-08-17 08:53:01 -0300

    Atualização dos métodos inserindo ResponseEntity (Erros 2xx, 4xx e 5xx)

--- src/main/java/br/com/etec/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/etec/ingresso/controller/FilmeController.java
@@ -3,6 +3,9 @@ package br.com.etec.ingresso.controller;
 import br.com.etec.ingresso.entity.Filme;
 import br.com.etec.ingresso.enuns.ClassificacaoIndicativaEnum;
 import br.com.etec.ingresso.enuns.SimNaoEnum;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,22 +31,43 @@ public class FilmeController {
                 .build();
         return List.of(filme1, filme2);
     }
+    List<Long> idsInexistentes = List.of(1L, 2L, 3L);
 
     @GetMapping("/{id}")
-    public Filme buscarPorId(@PathVariable Long id){
-        Filme filme1 = Filme.builder()
-                .id(id)
-                .nome("Matrix")
-                .classificacao(ClassificacaoIndicativaEnum.A16)
-                .emCartaz(SimNaoEnum.S)
-                .build();
-        return filme1;
+    public ResponseEntity<Filme> buscarPorId(@PathVariable Long id){
+        if(idsInexistentes.contains(id)) {
+            Filme filme1 = Filme.builder()
+                    .id(id)
+                    .nome("Matrix")
+                    .classificacao(ClassificacaoIndicativaEnum.A16)
+                    .emCartaz(SimNaoEnum.S)
+                    .build();
+            return ResponseEntity.ok((filme1));
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public Filme cadastrar(@RequestBody Filme filme){
+    public ResponseEntity<Filme> cadastrar(@RequestBody Filme filme){
         filme.setId(100L);
-        return filme;
+        return ResponseEntity.status(HttpStatus.CREATED).body(filme);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Filme> atualizar(@RequestBody Filme filme, @PathVariable Long id){
+        if(idsInexistentes.contains(id)){
+            return ResponseEntity.ok(filme);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        if(idsInexistentes.contains(id)){
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
 
